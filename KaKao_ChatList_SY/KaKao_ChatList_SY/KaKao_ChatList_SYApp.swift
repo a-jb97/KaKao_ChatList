79cab2d41e34462bb9fe35ae0b7c1ca938895092
Park Seo Yeon <syss220211@gmail.com>
2023-06-27 22:38:29 +0900

    서연 SwiftUI 프로젝트 생성

---
+++ KaKao_ChatList_SY/KaKao_ChatList_SY/KaKao_ChatList_SYApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaKao_ChatList_SYApp.swift
+//  KaKao_ChatList_SY
+//
+//  Created by 박서연 on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct KaKao_ChatList_SYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
