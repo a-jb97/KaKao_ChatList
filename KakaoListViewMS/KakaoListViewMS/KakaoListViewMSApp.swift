65cacd3ba4ad4a497d20e11016668eb8f0c8c85c
Jade <a_jb97@naver.com>
2023-06-27 23:47:44 +0900

    카카오톡 채팅 뷰 구현

---
+++ KakaoListViewMS/KakaoListViewMS/KakaoListViewMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakaoListViewMSApp.swift
+//  KakaoListViewMS
+//
+//  Created by 전민석 on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct KakaoListViewMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
